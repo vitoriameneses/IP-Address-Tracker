90064782e8b2af59ef07e7ae58d904b589abe6c8
Vitória Teixeira <vitoria.menesesmt@gmail.com>
2025-03-26 11:30:28 -0300

    feat: add ip tracker API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './components/map/map.component';
+import { FormsModule } from '@angular/forms';
+import { IpService } from './services/ip.service';
 
 @Component({
   selector: 'app-root',
-  imports: [MapComponent],
+  imports: [MapComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  ip_input: string = '';
+
+  ip_address: string = '';
+  location: string = '';
+  timezone: string = '';
+  isp: string = '';
+
   title = 'ip-address-tracker';
+
+  constructor(
+    private ipService: IpService
+  ){}
+
+  trackIP(ip: string) {
+    this.ipService.getIpAddress(ip).subscribe((data) => {
+      this.ip_address = data.ip;
+      this.location = `${data.location.city}, ${data.location.region}`;
+      this.timezone = data.location.timezone;
+      this.isp = data.isp;
+    });
+  }
 }
--- src/app/services/ip.service.ts
+++ src/app/services/ip.service.ts
@@ -6,7 +6,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class IpService {
-  private apiUrl = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_r2m0XZUnPDFVZm8X5QSPQvSkkmep7&ipAddress=8.8.8.8';
+  private apiUrl = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_r2m0XZUnPDFVZm8X5QSPQvSkkmep7&ipAddress=';
+
+  //'https://geo.ipify.org/api/v2/country,city?apiKey=at_r2m0XZUnPDFVZm8X5QSPQvSkkmep7&ipAddress=8.8.8.8';
+
   constructor(private http: HttpClient) { }
 
   getIpAddress(ip: string): Observable<any> {
