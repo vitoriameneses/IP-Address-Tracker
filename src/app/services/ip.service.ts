11aa4778efe040d9cad50b0c23ad1dfdeeb9d51b
Vitória Teixeira <vitoria.menesesmt@gmail.com>
2025-03-31 09:25:59 -0300

    fix enviroment variables

--- src/app/services/ip.service.ts
+++ src/app/services/ip.service.ts
@@ -6,13 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class IpService {
-  private baseUrl = 'https://geo.ipify.org/api/v2/country,city';
-
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getIpAddress(ip: string): Observable<any> {
-    console.log(`url enviada: /api/get-ip-info?ip=${ip}`);
     return this.http.get(`/api/get-ip-info?ip=${ip}`);
   }
 }
